c6374e876d9f91e438b647d07525952ef6678694
Nelson Villatoro <villatoro.g.nelson@gmail.com>
2024-10-16 07:10:23 -0400

    feat: Add JAVAPattern - Demonstrate ability to print pattern using simple spacing

---
+++ Personal-Java/JAVAPattern.java
@@ -0,0 +1,10 @@
+/* Write a program that prints the pattern J A V A */
+
+public class JAVAPattern {
+    public static void main(String[] args) {
+        System.out.println("    J     A     V     V    A     ");
+        System.out.println("    J    A A     V   V    A A    ");
+        System.out.println("J   J   AAAAA     V V    AAAAA   ");
+        System.out.println(" J J   A     A     V    A     A  ");
+    }
+}
